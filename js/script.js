c7fd80fbdd9314ee1b34872829f5e89ce60a286a
rtavaresramos <rtavareskxd99@gmail.com>
2020-05-21 16:03:21 -0300

    adding the newsite resposivity version

--- js/script.js
+++ js/script.js
@@ -3,3 +3,45 @@ var e = document.getElementById("toggle-control")
 function handleToggle(){
 e.classList.toggle("toggle-off")
 }
+
+var arrowLeft = document.getElementById("arrow-left")
+var arrowRight = document.getElementById("arrow-right")
+var projectContainer = document.getElementById("project-container")
+var rightMove = 0
+arrowLeft.addEventListener('click', ()=>{
+  if(rightMove>900){
+    arrowLeft.style.display = 'block'
+  }else{
+    arrowLeft.style.display = 'none'
+  }
+  if(rightMove<900){
+    arrowRight.style.display = 'none'
+  }else{
+    arrowRight.style.display = 'block'
+  }
+  if(rightMove == 0){
+    projectContainer.scrollLeft = rightMove
+  }else{
+  rightMove = rightMove - 900
+  projectContainer.scrollLeft = rightMove
+  }
+})
+
+arrowRight.addEventListener('click', ()=>{
+  if(rightMove>=0){
+    arrowLeft.style.display = 'block'
+  }else{
+    arrowLeft.style.display = 'none'
+  }
+  if(rightMove<900){
+    arrowRight.style.display = 'block'
+  }else{
+    arrowRight.style.display = 'none'
+  }
+  if(rightMove == 1800){
+    projectContainer.scrollLeft = rightMove
+  }else{
+  rightMove = rightMove + 900
+  projectContainer.scrollLeft = rightMove
+  }
+})
