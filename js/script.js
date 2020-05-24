b8b8c2ab40250567382f1a89d1363f0140dbce1e
rtavaresramos <rtavareskxd99@gmail.com>
2020-05-24 05:56:51 -0300

    adding the newsite new theme and animations

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ e.classList.toggle("toggle-off")
 var arrowLeft = document.getElementById("arrow-left")
 var arrowRight = document.getElementById("arrow-right")
 var projectContainer = document.getElementById("project-container")
+
+var allTopics1 = document.querySelector(".topic__container-1")
+var allTopics2 = document.querySelector(".topic__container-2")
+var allTopics3 = document.querySelector(".topic__container-3")
+var allTopics4 = document.querySelector(".topic__container-4")
+var button = document.querySelector(".call-to-action")
+
 var rightMove = 0
 arrowLeft.addEventListener('click', ()=>{
   if(rightMove>900){
@@ -46,3 +53,25 @@ arrowRight.addEventListener('click', ()=>{
   projectContainer.scrollLeft = rightMove
   }
 })
+
+window.addEventListener("scroll", function() {
+  var positionY = window.pageYOffset
+ if(positionY >= 200){
+   allTopics1.style.display = 'block'
+ }
+ if(positionY >= 500){
+   allTopics2.style.display = 'block'
+ }
+ if(positionY >= 800){
+   allTopics3.style.display = 'block'
+ }
+ if(positionY >= 1100){
+   allTopics4.style.display = 'block'
+   allTopics4.style.marginBottom = '30px'
+ }
+ if(positionY >= 1200){
+   allTopics4.style.display = 'block'
+   allTopics4.style.marginBottom = '30px'
+ }
+
+});
